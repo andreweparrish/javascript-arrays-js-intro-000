20a2db17ae3d1425ceeb35e6490b5248f0d86b72
Andrew Parrish <andrew.e.parrish@gmail.com>
2020-05-09 01:26:58 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,6 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
+var addElementToBeginningOfArray(chocolateBars, addElement) {
+  var moreChocolate = chocolateBars.shift(addElement);
+  return moreChocolate
+}
